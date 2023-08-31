f85d1e82b2d9648428c1add63580c0129826f03b
tianchao <909203360@qq.com>
2023-08-31 17:25:06 +0800

    添加聊天页面

--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -3,17 +3,137 @@ const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      title: "Hello111"
+      //键盘高度
+      keyboardHeight: 0,
+      //底部消息发送高度
+      bottomHeight: 0,
+      //滚动距离
+      scrollTop: 0,
+      userId: "",
+      //发送的消息
+      chatMsg: "",
+      msgList: [
+        {
+          botContent: "hello，请问我有什么可以帮助你的吗？",
+          recordId: 0,
+          titleId: 0,
+          userContent: "",
+          userId: 0
+        },
+        {
+          botContent: "",
+          recordId: 0,
+          titleId: 0,
+          userContent: "你好呀我想问你一件事",
+          userId: 0
+        }
+      ]
     };
   },
-  onLoad() {
+  updated() {
+    this.scrollToBottom();
   },
-  methods: {}
+  computed: {
+    windowHeight() {
+      return this.rpxTopx(common_vendor.index.getSystemInfoSync().windowHeight);
+    },
+    // 键盘弹起来的高度+发送框高度
+    inputHeight() {
+      return this.bottomHeight + this.keyboardHeight;
+    }
+  },
+  onLoad() {
+    common_vendor.index.onKeyboardHeightChange((res) => {
+      this.keyboardHeight = this.rpxTopx(res.height - 30);
+      if (this.keyboardHeight < 0)
+        this.keyboardHeight = 0;
+    });
+  },
+  onUnload() {
+    common_vendor.index.offKeyboardHeightChange();
+  },
+  methods: {
+    focus() {
+      this.scrollToBottom();
+    },
+    blur() {
+      this.scrollToBottom();
+    },
+    // px转换成rpx
+    rpxTopx(px) {
+      let deviceWidth = common_vendor.wx$1.getSystemInfoSync().windowWidth;
+      let rpx = 750 / deviceWidth * Number(px);
+      return Math.floor(rpx);
+    },
+    // 监视聊天发送栏高度
+    sendHeight() {
+      setTimeout(() => {
+        let query = common_vendor.index.createSelectorQuery();
+        query.select(".send-msg").boundingClientRect();
+        query.exec((res) => {
+          this.bottomHeight = this.rpxTopx(res[0].height);
+        });
+      }, 10);
+    },
+    // 滚动至聊天底部
+    scrollToBottom(e) {
+      setTimeout(() => {
+        let query = common_vendor.index.createSelectorQuery().in(this);
+        query.select("#scrollview").boundingClientRect();
+        query.select("#msglistview").boundingClientRect();
+        query.exec((res) => {
+          if (res[1].height > res[0].height) {
+            this.scrollTop = this.rpxTopx(res[1].height - res[0].height);
+          }
+        });
+      }, 15);
+    },
+    // 发送消息
+    handleSend() {
+      if (!this.chatMsg || !/^\s+$/.test(this.chatMsg)) {
+        let obj = {
+          botContent: "",
+          recordId: 0,
+          titleId: 0,
+          userContent: this.chatMsg,
+          userId: 0
+        };
+        this.msgList.push(obj);
+        this.chatMsg = "";
+        this.scrollToBottom();
+      } else {
+        this.$modal.showToast("不能发送空白消息");
+      }
+    }
+  }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-    a: common_vendor.t($data.title)
+    a: common_vendor.f($data.msgList, (item, index, i0) => {
+      return common_vendor.e({
+        a: item.userContent != ""
+      }, item.userContent != "" ? {
+        b: common_vendor.t(item.userContent)
+      } : {}, {
+        c: item.botContent != ""
+      }, item.botContent != "" ? {
+        d: common_vendor.t(item.botContent)
+      } : {}, {
+        e: index
+      });
+    }),
+    b: `${$options.windowHeight - $options.inputHeight}rpx`,
+    c: $data.scrollTop,
+    d: common_vendor.o((...args) => $options.handleSend && $options.handleSend(...args)),
+    e: common_vendor.o((...args) => $options.sendHeight && $options.sendHeight(...args)),
+    f: common_vendor.o((...args) => $options.focus && $options.focus(...args)),
+    g: common_vendor.o((...args) => $options.blur && $options.blur(...args)),
+    h: $data.chatMsg,
+    i: common_vendor.o(($event) => $data.chatMsg = $event.detail.value),
+    j: common_vendor.o((...args) => $options.handleSend && $options.handleSend(...args)),
+    k: `${$data.keyboardHeight}rpx`,
+    l: `${$options.inputHeight}rpx`
   };
 }
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/Users/huamo/project/DisplayProject/qa/pages/index/index.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-1cf27b2a"], ["__file", "/Users/huamo/project/DisplayProject/qa/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
