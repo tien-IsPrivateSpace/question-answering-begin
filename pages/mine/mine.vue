39fc71461295019dab0896f31bbd09975d250383
co2color <819109255@qq.com>
2023-09-05 23:03:39 +0800

    feat

--- pages/mine/mine.vue
+++ pages/mine/mine.vue
@@ -2,21 +2,21 @@
 
 <template>
   <div>
-    <header class="flex items-center">
+    <div class="flex items-center">
       <img src="@/static/logo.png" />
       <div class="flex-col header-spans">
         <span class="font-lg">超大佬</span>
         <span class="font-base">超大佬的小程序</span>
       </div>
-    </header>
-    <main>
+    </div>
+    <div>
       <h3 v-for="item in 50">列表{{ item }}</h3>
-    </main>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-header {
+.header {
   padding: 20rpx;
   background: -webkit-linear-gradient(315deg, #e7f8ff 25%, #d8f4f9);
 
