dc87d0eb9077990a76a0c995480b7aefb506fa2f
co2color <819109255@qq.com>
2023-09-05 23:10:18 +0800

    feat: request test

--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -1,19 +1,37 @@
 "use strict";
 const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
-  data() {
-    return {
-      title: "Hello"
+  __name: "index",
+  setup(__props) {
+    function test() {
+      const requestTask = common_vendor.index.request({
+        url: "http://n4mt5q.natappfree.cc/question",
+        timeout: 15e3,
+        responseType: "text",
+        method: "POST",
+        enableChunked: true,
+        data: {
+          question: "how are you?"
+        },
+        success: (response) => {
+          console.log(response);
+        },
+        fail: (error) => {
+        }
+      });
+      requestTask.onHeadersReceived(function(res) {
+        console.log(res);
+      });
+      requestTask.onChunkReceived(function(res) {
+        console.log(res);
+      });
+    }
+    return (_ctx, _cache) => {
+      return {
+        a: common_vendor.o(test)
+      };
     };
-  },
-  onLoad() {
-  },
-  methods: {}
+  }
 };
-function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {
-    a: common_vendor.t($data.title)
-  };
-}
-const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__file", "/Users/huamo/project/DisplayProject/qa/pages/index/index.vue"]]);
+const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["__file", "C:/Users/81910/Desktop/code/js_code/question-answering-begin/pages/index/index.vue"]]);
 wx.createPage(MiniProgramPage);
